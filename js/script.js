4ba96f005a05b85b9af73013255bd47d07b7315e
lunaarien <luna.denice.06232003@gmail.com>
2025-01-17 15:11:54 +0800

    landing page

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-icon').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide the menu
+});
